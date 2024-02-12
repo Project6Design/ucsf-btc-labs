31a246b3bf3fce223cd8f498f1bcbf9f2e529e4e
Miguel Castillo <miguel@project6.com>
2024-02-12 16:31:05 -0600

    Apply drupal update and add redirect

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -23,3 +23,39 @@ $local_settings = __DIR__ . "/settings.local.php";
 if (file_exists($local_settings)) {
   include $local_settings;
 }
+
+$redirect_targets = [
+  'live-btc-francis.pantheonsite.io' => 'francislab.ucsf.edu',
+  'live-btc-lu.pantheonsite.io' => 'lulab.ucsf.edu'
+];
+
+
+if (isset($_ENV['PANTHEON_ENVIRONMENT']) && php_sapi_name() != 'cli' && array_key_exists($_SERVER['REQUEST_URI'], $redirect_targets)) {
+  // Redirect to https://$primary_domain in the Live environment
+  if ($_ENV['PANTHEON_ENVIRONMENT'] === 'live') {
+    // Replace www.example.com with your registered domain name.
+    $primary_domain = $redirect_targets[$_SERVER['REQUEST_URI']];
+  }
+  else {
+    // Redirect to HTTPS on every Pantheon environment.
+    $primary_domain = $_SERVER['HTTP_HOST'];
+  }
+
+  $requires_redirect = FALSE;
+
+  // Ensure the site is being served from the primary domain.
+  if ($_SERVER['HTTP_HOST'] != $primary_domain) {
+    $requires_redirect = TRUE;
+  }
+
+  if ($requires_redirect === TRUE) {
+    // Name transaction "redirect" in New Relic for improved reporting (optional).
+    if (extension_loaded('newrelic')) {
+      newrelic_name_transaction("redirect");
+    }
+
+    header('HTTP/1.0 301 Moved Permanently');
+    header('Location: https://' . $primary_domain . $_SERVER['REQUEST_URI']);
+    exit();
+  }
+}
